1f3cd35f12dfec14ff043994a15c4a4584ffd6ac
Satyam <satyamshukla20109211@gmail.com>
2026-09-25 13:36:05 +0530

    Fix Elementa cache

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,50 @@
-"use strict";
+const CACHE_NAME = "elementa-v2";
 
-const CACHE_NAME = "elementa-v1";
-
-const APP_FILES = [
-    "./",
-    "./index.html",
-    "./manifest.json",
-    "./css/style.css",
-    "./data/elements.js",
-    "./js/theme.js",
-    "./js/search.js",
-    "./js/filters.js",
-    "./js/details.js",
-    "./js/app.js"
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./data/elements.js",
+  "./js/theme.js",
+  "./js/search.js",
+  "./js/filters.js",
+  "./js/details.js",
+  "./js/app.js",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                return cache.addAll(APP_FILES);
-            })
-            .then(function () {
-                return self.skipWaiting();
-            })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches.keys()
-            .then(function (cacheNames) {
-                return Promise.all(
-                    cacheNames
-                        .filter(function (name) {
-                            return name !== CACHE_NAME;
-                        })
-                        .map(function (name) {
-                            return caches.delete(name);
-                        })
-                );
-            })
-            .then(function () {
-                return self.clients.claim();
-            })
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (cachedResponse) {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
 
-                return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
